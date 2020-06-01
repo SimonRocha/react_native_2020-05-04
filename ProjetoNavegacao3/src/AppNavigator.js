63dc1fec6e659152ae08dfd36d844026fbb99d3e
SimonRocha <simon.henrique.rocha@gmail.com>
2020-06-01 19:27:25 -0300

    Bottom Tab Navigator

--- ProjetoNavegacao3/src/AppNavigator.js
+++ ProjetoNavegacao3/src/AppNavigator.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import { createStackNavigator } from '@react-navigation/stack';
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
 
 import Home from './screens/Home';
 import TelaA from './screens/TelaA';
@@ -12,17 +13,21 @@ import DetalheCard5 from './screens/DetalheCard5'
 
 
 const Stack = createStackNavigator();
+const Tab = createMaterialBottomTabNavigator();
 
 export default function AppNavigator() {
   return (
-    <Stack.Navigator>
-      <Stack.Screen name="ListExemplo" component={ListExemplo} />
-      <Stack.Screen name="Home" component={Home} />
-      <Stack.Screen name="TelaA" component={TelaA} />
-      <Stack.Screen name="TelaB" component={TelaB} />
-      <Stack.Screen name="TelaC" component={TelaC} />
+    <Tab.Navigator 
+      activeColor="#f0edf6"
+      inactiveColor="#3e2465"
+      barStyle={{ backgroundColor: '#694fad' }}
+    >
+      <Tab.Screen name="Home" component={Home} />
+      <Tab.Screen name="ListExemplo" component={ListExemplo} />
+      <Tab.Screen name="TelaA" component={TelaA} />
+      <Tab.Screen name="TelaB" component={TelaB} />
+      <Tab.Screen name="TelaC" component={TelaC} />
       
-      <Stack.Screen name="DetalheCard5" component={DetalheCard5} />
-    </Stack.Navigator>
+    </Tab.Navigator>
   );
 }
