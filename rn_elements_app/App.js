54d4856608f789b3ef4192e5f0f39463d0e91328
SimonRocha <simon.henrique.rocha@gmail.com>
2020-06-10 20:26:59 -0300

    Exercicio 13 parte 1

--- rn_elements_app/App.js
+++ rn_elements_app/App.js
@@ -7,14 +7,26 @@
  */
 
 import React from 'react';
+import { createDrawerNavigator } from '@react-navigation/drawer'
+import { NavigationContainer } from '@react-navigation/native';
+
 
 import TelaA from './src/screens/TelaA'
+import TelaB from './src/screens/TelaB'
+import TelaC from './src/screens/TelaC'
+
+const Drawer = createDrawerNavigator();
 
 const App: () => React$Node = () => {
   return (
-    <TelaA>
-      
-    </TelaA>
+    <NavigationContainer>
+     <Drawer.Navigator initialRouteName="TelaA">
+      <Drawer.Screen name="TelaA" component={TelaA} />
+      <Drawer.Screen name="TelaB" component={TelaB} />
+      <Drawer.Screen name="TelaC" component={TelaC} />
+     </Drawer.Navigator>
+   </NavigationContainer>
+
   );
 };
 
--- rn_elements_app/src/screens/TelaB.js
+++ rn_elements_app/src/screens/TelaB.js
@@ -3,16 +3,15 @@ import { View } from 'react-native'
 import { Button } from 'react-native-elements'
 import Icon from 'react-native-vector-icons/FontAwesome';
 
-export default class TelaA extends React.Component {
+export default class TelaB extends React.Component {
     render() {
         return(
             <Button
                 buttonStyle={{ backgroundColor: '#000' }}
                 icon={ <Icon name="arrow-right" size={15} color="white"/> }
-                title="Tela B"
-                onPress={() => this.props.navigation.navigate('TelaB')}
+                title="Tela C"
+                onPress={() => this.props.navigation.navigate('TelaC')}
             />
-
         )
     }
 }
